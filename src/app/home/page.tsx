45efe28932c5234d74ce500c0d08ed5cfeb8515e
Gust4voSales <manoel0gustavo@gmail.com>
2023-04-15 11:45:16 -0300

    feat: add basic structure

--- src/app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AudioRecorderCard } from "@components/AudioRecorderCard";
-
-export default function Home() {
-  return (
-    <div className="h-screen flex place-items-center place-content-center">
-      <AudioRecorderCard />
-    </div>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { AudioPlayerCard } from "@src/components/AudioPlayerCard";
 import useLoopsStore from "@stores/LoopAudio";
 import { STATUS } from "@stores/LoopAudio/LoopAudio";
 import { AudioRecorderCard } from "@components/AudioRecorderCard";
+import { Info } from "@phosphor-icons/react";
 
 export default function Home() {
   const loopsAudioStore = useLoopsStore();
@@ -13,17 +14,37 @@ export default function Home() {
   const [useExpressionDetector, setUseExpressionDetector] = useState(false);
 
   return (
-    <div className="prose flex flex-col gap-4">
-      <AudioRecorderCard />
-      {loopsAudioStore.audios.map((loop) => (
-        <AudioPlayerCard audioLoop={loop} key={loop.name} />
-      ))}
+    <div className="w-full max-w-5xl mx-auto p-4 mt-4 bg-base-300 rounded-[var(--rounded-box)]">
+      <div className="flex flex-col gap-4 items-center w-full">
+        <AudioRecorderCard />
 
-      <button onClick={() => setUseExpressionDetector(!useExpressionDetector)} className="btn">
-        CONTROLE POR WEBCAM
-      </button>
+        <div className="divider">
+          <div className="prose">
+            <h3>LOOPS</h3>
+          </div>
+        </div>
 
-      {useExpressionDetector && <ExpressionDetectorCam />}
+        <div className="w-full p-1 mx-auto flex gap-2 flex-wrap">
+          {loopsAudioStore.audios.map((loop) => (
+            <AudioPlayerCard audioLoop={loop} key={loop.name} />
+          ))}
+
+          {loopsAudioStore.audios.length === 0 && (
+            <div className="alert shadow-lg ">
+              <div className="prose mx-auto">
+                <Info size={24} weight="fill" />
+                <span>Nenhum loop gravado ainda.</span>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* <button onClick={() => setUseExpressionDetector(!useExpressionDetector)} className="btn w-min">
+        WEBCAM
+      </button> */}
+
+        {useExpressionDetector && <ExpressionDetectorCam />}
+      </div>
     </div>
   );
 }
