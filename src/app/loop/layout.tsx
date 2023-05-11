7fd45f42b7b22ce99de7433d6d643b7feebafe7d
Gust4voSales <manoel0gustavo@gmail.com>
2023-05-11 19:22:25 -0300

    fix: show custom toast styles

--- src/app/loop/layout.tsx
+++ src/app/loop/layout.tsx
@@ -11,7 +11,7 @@ export default function LoopLayout({ children }: { children: React.ReactNode }) 
       <Toaster
         position="top-right"
         toastOptions={{
-          className: "bg-base-300 text-base-content max-w-md",
+          className: "!bg-base-300 !text-base-content !max-w-md",
           error: {
             iconTheme: {
               primary: "hsl(var(--er))",
